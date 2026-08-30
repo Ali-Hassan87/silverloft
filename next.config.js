6f24bd4ab395dbbd6304a8f34532e9891239e332
ALI HASSAN <romanempireewq@gmail.com>
2026-08-30 20:47:43 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
   images: {
-    remotePatterns: []
-  }
+    unoptimized: true,
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
